28c5259877037457d11831087f30cbc598345200
LimonJohn <mikhalyov.alexey.sergeevich@gmail.com>
2021-01-13 03:58:34 +0300

    -

--- Exercises_python/Phase_1/1.7.py
+++ Exercises_python/Phase_1/1.7.py
@@ -3,6 +3,5 @@ import math
 a = int(input('Класс a'))
 b = int(input('Класс b'))
 c = int(input('Класс c'))
-
 t = math.ceil(a / 2) + math.ceil(b / 2) + math.ceil(c / 2)
 print(t)
--- Exercises_python/Phase_1/2.2.py
+++ Exercises_python/Phase_1/2.2.py
@@ -1,2 +1,8 @@
-# 2.2 «Знак числа»
+# 2.2 Знак числа
 x = int(input())
+if x > 0:
+    print(1)
+elif x < 0:
+    print(-1)
+elif x == 0:
+    print(0)
