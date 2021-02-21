236b65ab154873c894a331432b89267400eec327
LimonJohn <mikhalyov.alexey.sergeevich@gmail.com>
2021-02-21 20:21:06 +0300

    -

--- Exercises_python/Phase_1/3.16.py
+++ Exercises_python/Phase_1/3.16.py
@@ -1,1 +1,9 @@
 # 3.16 Задача «Проценты»
+from math import floor
+P = int(input())
+X = int(input())
+Y = int(input())
+babosiki = (X * 100 + Y) + (X * 100 + Y) * P / 100
+rub = int(babosiki // 100)
+kop = floor(babosiki % 100)
+print(rub, kop)
