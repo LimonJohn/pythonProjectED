ed15bfbab58a93f22a7beb263c77db39db823923
LimonJohn <mikhalyov.alexey.sergeevich@gmail.com>
2021-02-27 23:29:48 +0300

    -

--- Exercises_python/Phase_1/2.5.py
+++ Exercises_python/Phase_1/2.5.py
@@ -1,5 +1,16 @@
 # 2.5 Минимум из трех чисел
+'''
 a = []
 for i in range(3):
     a.append(int(input()))
 print(min(a))
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+if a <= b and a <= c:
+    print(a)
+elif b <= a and b <= c:
+    print(b)
+else:
+    print(c)
