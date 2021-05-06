458b95f2a1f88123801b47c2acd0598d43d2c768
LimonJohn <mihalyov.alexey.sergeevich@gmail.com>
2021-05-06 18:08:13 +0300

    -

--- Exercises_python/Phase_1/7.2.py
+++ Exercises_python/Phase_1/7.2.py
@@ -1,4 +1,4 @@
 # 7.2 Задача «Четные элементы»
-digits = [int(digits) for digits in input().split()]
-evenElem = [digits for digits in digits if digits % 2 == 0]
-print(' '.join([str(i) for i in evenElem]))
+digits = [int(digits) for digits in input().split() if int(digits) % 2 == 0]
+# evenElem = [digits for digits in digits if digits % 2 == 0]
+print(' '.join([str(i) for i in digits]))
